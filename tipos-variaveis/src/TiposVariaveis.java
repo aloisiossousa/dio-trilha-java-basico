f60f09573cd39aaa5f469e6643e730dcb8d23e26
Aloisio S Sousa <aloisio.ssousa@gmail.com>
2023-03-22 21:14:07 -0300

    Teste tipo inteiro ou int ou integer

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,11 @@
 public class TiposVariaveis {
-    
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 20;
+
+        int soma = (nota1 + nota2);
+
+        System.out.println(soma);
+
+    }
 }
