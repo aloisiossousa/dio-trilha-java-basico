06ae862164d0716b15b2db2807ed1a8ac76384ec
Aloisio S Sousa <aloisio.ssousa@gmail.com>
2023-03-22 21:58:22 -0300

    Operadores concantenação

--- tipos-variaveis/src/edu/operadores/Operadores.java
+++ tipos-variaveis/src/edu/operadores/Operadores.java
@@ -2,8 +2,18 @@ package edu.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM " + "JAVA";
+       // String nomeCompleto = "LINGUAGEM " + "JAVA";
 
-        System.out.println(nomeCompleto);
+       String concatenacao = "?";
+
+       concatenacao = 1+1+1+"1";
+       System.out.println(concatenacao);
+
+       concatenacao = 1+1+"1"+1;
+       System.out.println(concatenacao);
+       
+       concatenacao = "1"+(1+1+1+1);
+    
+       System.out.println(concatenacao);
     }
 }
